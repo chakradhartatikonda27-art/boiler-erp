adc7597e20fea33db66a07077f8db2dab6e92c94
siyantech <siyantechglobal@gmail.com>
2026-09-19 23:03:33 +0530

    Include DashboardLayout with Sidebar and Header on root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import DashboardLayout from './(dashboard)/layout';
 import DashboardPage from './(dashboard)/dashboard/page';
 
 export default function Home() {
-  return <DashboardPage />;
+  return (
+    <DashboardLayout>
+      <DashboardPage />
+    </DashboardLayout>
+  );
 }
+
